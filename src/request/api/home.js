7228b68e28348bfc3e73f9eaff2e9f4e72150057
kevin <kevin@MAir.local>
2023-03-07 17:45:07 +0800

    登录

--- src/request/api/home.js
+++ src/request/api/home.js
@@ -1,15 +1,22 @@
 import service from '..'
 
 // 获取首页轮播数据
-export function getBanner(){
+export function getBanner() {
   return service.get('/banner?type=2')
 }
 
-export function getMusicList(){
+export function getMusicList() {
   return service.get('/personalized?limit=10')
 }
 
-export function getSearchMusic(data){
+export function getSearchMusic(data) {
   return service.get(`/search?keywords=${data}`)
 }
 
+export function getPhoneLogin(data) {
+  return service.get(`/login/cellphone?phone=${data.phone}&password=${data.password}`)
+}
+
+export function getLoginUser(data) {
+  return service.get(`/user/detail?uid=${data}`)
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-
+import store from '../store/index.js';
 const routes = [
   {
     path: '/',
@@ -10,7 +10,7 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    component: () => import( '../views/AboutView.vue'),
+    component: () => import('../views/AboutView.vue'),
   },
   {
     path: '/itemMusic',
@@ -18,9 +18,26 @@ const routes = [
     component: () => import('../views/ItemMusic.vue'),
   },
   {
-    path:'/search',
-    name:'Search',
+    path: '/search',
+    name: 'Search',
     component: () => import('../views/Search.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/infoUser',
+    name: 'InfoUser',
+    beforeEnter: (to, from, next) => {
+      if (store.state.isLogin) {
+        next();
+      } else {
+        next('/login');
+      }
+    },
+    component: () => import('../views/InfoUser.vue')
   }
 ];
 
@@ -28,5 +45,13 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+
+router.beforeEach((to, from) => {
+  if (to.path == '/login' || store.state.token || localStorage.getItem('token')) {
+    store.state.isFooterMusic = false
+  } else {
+    store.state.isFooterMusic = true
+  }
+})
 
 export default router;
