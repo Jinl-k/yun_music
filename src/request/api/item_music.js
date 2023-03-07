15647dbc002310cf95c27f6df2cb4ae96ae92c10
kevin <kevin@MAir.local>
2023-03-07 15:06:40 +0800

    歌词

--- src/request/api/item_music.js
+++ src/request/api/item_music.js
@@ -8,3 +8,7 @@ export function getMusicItemList(id) {
   return service.get(`/playlist/track/all?id=${id}&limit=20&offset=0`)
 }
 
+export function getMusicLyric(data){
+  return service.get(`/lyric?id=${data}`)
+}
+
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex';
+import { getMusicLyric } from '../request/api/item_music';
 
 export default createStore({
   state: {
@@ -16,15 +17,30 @@ export default createStore({
         id: 1446137141,
         name: '雨爱（抖音版）',
         ar: [{ name: '灏灏灏仔' }],
+        currentTime: 0, // 当前播放时间
+        duration: 0, // 歌曲总时长
       },
     ],
     playListIndex: 0, //默认下标为0
     isbtnShow: true, //是否显示暂停按钮
+    detailShow: false, //是否显示歌曲详情
   },
   getters: {},
   mutations: {
     updateIsbtnShow: (state, value) => (state.isbtnShow = value),
+    updatePlayList: (state, value) => (state.playList = value),
+    updatePlayListIndex: (state, value) => (state.playListIndex = value),
+    updateDetailShow: (state) => (state.detailShow = !state.detailShow),
+    updateLyricList: (state, value) => (state.lyricList = value),
+    updateCurrentTime: (state, value) => (state.currentTime = value),
+    updateDuration: (state, value) => (state.duration = value),
   },
-  actions: {},
+  actions: {
+    getLyric: async (context, value) => {
+      let res = await getMusicLyric(value)
+      // 触发修改歌词
+      context.commit('updateLyricList', res.data.lrc)
+    },
+  },
   modules: {},
 });
